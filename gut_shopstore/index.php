ab7437d1bc9409856354c504b19e9486ee023c98
wajihahmansor <38195429+wajihahmansor@users.noreply.github.com>
2018-04-16 11:17:30 +0800

    Create index.php

---
+++ gut_shopstore/index.php
@@ -0,0 +1,229 @@
+<?php include 'includes/layout/head.php'; ?>
+<body class="animsition">
+
+<!-- Header -->
+<header class="header1">
+<!-- Header desktop -->
+<div class="container-menu-header">
+
+<?php include 'includes/layout/topbar.php'; ?>
+<?php include 'includes/layout/navbar-left.php'; ?>
+</div>
+<?php include 'includes/layout/navbar-right.php'; ?>
+<?php include 'includes/layout/slider.php'; ?>
+
+<div style="padding:3%;"></div>
+<!-- Banner -->
+<section class="banner bgwhite p-t-40 p-b-40">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="img/category/cupcake.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="cupcake-shop.php" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+							Cupcake
+						</a>
+					</div>
+				</div>
+
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="img/category/cake.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="cake-shop.php" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+							Cake
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="img/category/macaron.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="macaron-shop.php" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+							Macaron
+						</a>
+					</div>
+				</div>
+
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="img/category/crepe.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="crepe-shop.php" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+							Crepe
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="img/category/donut.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="donut-shop.php" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+							Donut
+						</a>
+					</div>
+				</div>
+
+				<!-- block2 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="img/category/bread.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="bread-shop.php" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+							Bread
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Instagram -->
+<section class="instagram p-t-20">
+<div class="sec-title p-b-52 p-l-15 p-r-15">
+	<h3 class="m-text5 t-center">
+		@ follow us on Instagram
+	</h3>
+</div>
+
+<div class="flex-w">
+	<!-- Block4 -->
+	<div class="block4 wrap-pic-w">
+		<img src="img/d1.jpg" alt="IMG-INSTAGRAM">
+
+		<a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+			<span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+				<i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+				<span class="p-t-2">39</span>
+			</span>
+
+			<div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+				<p class="s-text10 m-b-15 h-size1 of-hidden">
+					Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
+				</p>
+
+				<span class="s-text9">
+					Photo by @nancyward
+				</span>
+			</div>
+		</a>
+	</div>
+
+	<!-- Block4 -->
+	<div class="block4 wrap-pic-w">
+		<img src="img/d2.jpg" alt="IMG-INSTAGRAM">
+
+		<a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+			<span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+				<i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+				<span class="p-t-2">39</span>
+			</span>
+
+			<div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+				<p class="s-text10 m-b-15 h-size1 of-hidden">
+					Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
+				</p>
+
+				<span class="s-text9">
+					Photo by @nancyward
+				</span>
+			</div>
+		</a>
+	</div>
+
+	<!-- Block4 -->
+	<div class="block4 wrap-pic-w">
+		<img src="img/d3.jpg" alt="IMG-INSTAGRAM">
+
+		<a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+			<span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+				<i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+				<span class="p-t-2">39</span>
+			</span>
+
+			<div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+				<p class="s-text10 m-b-15 h-size1 of-hidden">
+					Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
+				</p>
+
+				<span class="s-text9">
+					Photo by @nancyward
+				</span>
+			</div>
+		</a>
+	</div>
+
+	<!-- Block4 -->
+	<div class="block4 wrap-pic-w">
+		<img src="img/d4.jpg" alt="IMG-INSTAGRAM">
+
+		<a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+			<span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+				<i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+				<span class="p-t-2">39</span>
+			</span>
+
+			<div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+				<p class="s-text10 m-b-15 h-size1 of-hidden">
+					Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
+				</p>
+
+				<span class="s-text9">
+					Photo by @nancyward
+				</span>
+			</div>
+		</a>
+	</div>
+
+	<!-- Block4 -->
+	<div class="block4 wrap-pic-w">
+		<img src="img/d5.jpg" alt="IMG-INSTAGRAM">
+
+		<a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
+			<span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
+				<i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
+				<span class="p-t-2">39</span>
+			</span>
+
+			<div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
+				<p class="s-text10 m-b-15 h-size1 of-hidden">
+					Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
+				</p>
+
+				<span class="s-text9">
+					Photo by @nancyward
+				</span>
+			</div>
+		</a>
+	</div>
+</div>
+</section>
+
+<!-- Shipping -->
+<section class="shipping bgwhite p-t-62 p-b-46">
+<div class="flex-w p-l-15 p-r-15">
+
+</div>
+</section>
+
+<?php include 'includes/layout/footer.php' ?>
